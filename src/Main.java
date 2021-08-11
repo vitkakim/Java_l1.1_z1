6510c23f4e137f20a0cc607d9c7c0f83acf078c0
Victroria Anisimova <vitkakim@mail.ru>
2021-08-11 19:39:07 +0300

    4th card

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "4147569377465216";
+        String number = "4036031381680433";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
